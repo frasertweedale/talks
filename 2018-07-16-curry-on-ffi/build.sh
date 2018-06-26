30a48e5a227a1dd2113df5868fa6906854c0c14a
Fraser Tweedale <frase@frase.id.au>
2018-06-26 15:48:27 +1000

    curry-on-ffi: add talk

---
+++ 2018-07-16-curry-on-ffi/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+SVG_CMD() {
+  [ -z "$2" ] && DIR=. || DIR=$2
+  inkscape -D -z --file="${DIR}/$1.svg" --export-pdf="$1-ARTIFACT.pdf" --export-latex
+}
+
+SVG_CMD cc-by ../share
+
+pdflatex slides
