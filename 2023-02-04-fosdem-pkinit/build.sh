cae050a33684a110a738090ef1a1173c2dfa9687
Fraser Tweedale <frase@frase.id.au>
2023-02-04 12:48:02 +0100

    fosdem-pkinit: add talk

---
+++ 2023-02-04-fosdem-pkinit/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+. ../share/subr.sh
+#SVG_CMD cc-by ../share
+#SVG_CMD Logo-RedHat-A-Color-RGB ../share
+#SVG_CMD kerb-ap-req-post img
+#SVG_CMD kerb-ap-req img
+#SVG_CMD kerb-as-exchange-post img
+#SVG_CMD kerb-as-rep img
+#SVG_CMD kerb-as-req img
+#SVG_CMD kerb-parties img
+#SVG_CMD kerb-tgs-rep-post img
+#SVG_CMD kerb-tgs-rep img
+#SVG_CMD kerb-tgs-req img
+#SVG_CMD kerb-pkinit-as-rep img
+#SVG_CMD kerb-pkinit-as-req img
+#SVG_CMD kerb-pkinit-as-exchange-post img
+pdflatex slides
