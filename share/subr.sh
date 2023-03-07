a2317a17646cbcd073516d191547eade388fe18e
Fraser Tweedale <frase@frase.id.au>
2023-03-07 12:59:52 +1000

    share: avoid unnecessary inkscape invocations

--- share/subr.sh
+++ share/subr.sh
@@ -1,4 +1,7 @@
 SVG_CMD() {
   [ -z "$2" ] && DIR=. || DIR=$2
-  inkscape -D -z --file="${DIR}/$1.svg" --export-pdf="$1-ARTIFACT.pdf" --export-latex
+  IN="${DIR}/$1.svg"
+  OUT="$1-ARTIFACT.pdf"
+  [ ! -e $OUT -o $IN -nt $OUT ] \
+    && inkscape -D -z --file=$IN --export-pdf=$OUT --export-latex
 }
